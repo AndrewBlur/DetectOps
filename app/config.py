fc5ffa6783059b5e6951cd3932b1340337af636a
[Andrew A] <[andrewgtc2005@gmail.com]>
2025-12-01 20:07:28 +0530

    # Fixed
     - unwanted settings attributes in DBSettings

--- app/config.py
+++ app/config.py
@@ -9,9 +9,6 @@ class DBSettings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
     DB_NAME: str
-    SECRET_KEY: str
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     model_config = ConfigDict(env_file="../.env")
 
